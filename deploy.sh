e03a40442bbda481f4c89ad04a588866aec7e07a
dx <dong.xing@outlook.com>
2020-03-09 17:44:32 +0800

    移除手动部署至github pages，添加自动部署脚本

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下
+cd dist
+
+git init
+git add -A
+git commit -m '部署'
+
+
+# 部署到 https://WX-DongXing.github.io/terminator
+git push -f git@github.com:WX-DongXing/terminator.git master:gh-pages
+
+cd -
